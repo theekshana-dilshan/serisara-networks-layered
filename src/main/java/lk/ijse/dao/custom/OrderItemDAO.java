ffa7db924b7ec56c4fb8e66273f389942e68862f
theekshana-dilshan <sptheekshanadilshan@gmail.com>
2024-01-11 22:14:24 +0530

    Updated

--- src/main/java/lk/ijse/dao/custom/OrderItemDAO.java
+++ src/main/java/lk/ijse/dao/custom/OrderItemDAO.java
@@ -1,4 +1,15 @@
 package lk.ijse.dao.custom;
 
-public interface OrderItemDAO {
+import lk.ijse.dao.SuperDAO;
+import lk.ijse.db.DBConnection;
+import lk.ijse.dto.tm.OrderTm;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface OrderItemDAO extends SuperDAO {
+    boolean saveOrderDetails(String orderId, OrderTm tm) throws SQLException, ClassNotFoundException;
+    boolean saveOrderDetails(String orderId, List<OrderTm> cartTmList) throws SQLException, ClassNotFoundException;
 }
--- src/main/java/lk/ijse/dao/custom/impl/OrderItemDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/OrderItemDAOImpl.java
@@ -1,4 +1,26 @@
 package lk.ijse.dao.custom.impl;
 
-public class OrderItemDAOImpl {
+import lk.ijse.dao.SQLUtil;
+import lk.ijse.dao.custom.OrderItemDAO;
+import lk.ijse.dto.tm.OrderTm;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class OrderItemDAOImpl implements OrderItemDAO {
+    @Override
+    public boolean saveOrderDetails(String orderId, OrderTm tm) throws SQLException, ClassNotFoundException {
+        return SQLUtil.execute("INSERT INTO orderItemDetail VALUES(?, ?, ?, ?)",
+                tm.getQty(), tm.getUnitPrice(), tm.getItemId(), orderId);
+    }
+
+    @Override
+    public boolean saveOrderDetails(String orderId, List<OrderTm> cartTmList) throws SQLException, ClassNotFoundException {
+        for(OrderTm tm : cartTmList) {
+            if(!saveOrderDetails(orderId, tm)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
